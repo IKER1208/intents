ab6194afefdcd4bd0fbf178e35f8d49dff5cfc10
IKER1208 <iker34267@gmail.com>
2024-10-22 13:57:58 -0600

    nuevo intent

--- app/src/main/java/com/example/intents/MainActivity.java
+++ app/src/main/java/com/example/intents/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -16,11 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Habilitar Edge to Edge
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Aplicar Window Insets para manejar los márgenes con las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -43,12 +43,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        // 3. Botón para abrir la misma actividad con un mensaje (no funciona;()
-        Button btnAbrirConMensaje = findViewById(R.id.btnAbrirConMensaje);
-        btnAbrirConMensaje.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            intent.putExtra("mensaje", "Hola desde la misma actividad!");
-            startActivity(intent);
+        // 3. boton pra abrir un toast
+        Button btnMostrarToast = findViewById(R.id.btnAbrirConMensaje);
+        btnMostrarToast.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "HOLA", Toast.LENGTH_SHORT).show();
         });
 
         // ------------------ Intents Implícitos ------------------
